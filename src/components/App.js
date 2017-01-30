782acb08380779a2a4bbd30c34eba2ec3e9e9e46
Michon van Dooren <michon1992@gmail.com>
2017-01-30 23:36:38 +0100

    Switched over to react-router.

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,40 @@
 import React from 'react';
 import Relay from 'react-relay';
 import { Grid, Row, Col, Navbar, Nav, NavItem } from 'react-bootstrap';
-import Query from '../queries/Query';
+import { Router, Route, IndexRoute, hashHistory, applyRouterMiddleware } from 'react-router';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+import useRelay from 'react-router-relay';
+
 import Teas from './Teas';
 
-const CONTAINERS = {
-	teas: Teas,
-}
+const Queries = {
+	viewer: () => Relay.QL`
+		query {
+			query
+		}
+	`,
+};
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			page: 'teas',
-		}
-	}
-
-	onSelect(eventKey) {
-		this.setState({
-			page: eventKey,
-		});
-	}
-
 	render() {
 		return (
 			<div>
-				<Navbar onSelect={this.onSelect.bind(this)}>
+				<Navbar>
 					<Navbar.Header>
 						<Navbar.Brand>
 							<a href="#">Tea Time</a>
 						</Navbar.Brand>
 					</Navbar.Header>
 					<Nav>
-						<NavItem eventKey={'teas'} href="#">Teas</NavItem>
+						<IndexLinkContainer to={{ pathname: '/' }}>
+							<NavItem href="#">Teas</NavItem>
+						</IndexLinkContainer>
 					</Nav>
 				</Navbar>
 				<Grid>
 					<Row>
 						<Col xs={12} md={12}>
-							<Relay.Renderer
-								environment={Relay.Store}
-								Container={CONTAINERS[this.state.page]}
-								queryConfig={new Query()}
-							/>
+							{this.props.children}
 						</Col>
 					</Row>
 				</Grid>
@@ -52,4 +43,14 @@ class App extends React.Component {
 	}
 };
 
-export default App;
+export default function() {
+	return <Router
+		history={hashHistory}
+	    render={applyRouterMiddleware(useRelay)}
+		environment={Relay.Store}
+	>
+		<Route path="/" component={App}>
+			<IndexRoute component={Teas} queries={Queries} />
+		</Route>
+	</Router>
+};
